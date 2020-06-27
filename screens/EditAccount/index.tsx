8476d145009cac6ecd2c7c1e8956ffeb1a010e93
Nethan <nethan.tran@gmail.com>
2020-06-27 19:00:59 +1000

    create account form

--- screens/EditAccount/index.tsx
+++ screens/EditAccount/index.tsx
@@ -3,12 +3,16 @@ import React from "react";
 import { StyleSheet, Text, View, TextInput, ScrollView } from "react-native";
 import { Input } from "react-native-elements";
 import DropDownPicker from "react-native-dropdown-picker";
+import { useFormik } from "formik";
 
-const FormInput = ({ title, value }) => {
+import { auStates, schema } from "./schema";
+
+const FormInput = ({ title, value, error, showError, onChange, onBlur }) => {
   return (
     <>
       <Text>{title}</Text>
-      <Input value={value} />
+      <Input value={value} onChangeText={onChange} onBlur={onBlur} />
+      {!!showError ? <Text style={{ color: "maroon" }}>{error}</Text> : null}
     </>
   );
 };
@@ -16,30 +20,76 @@ const FormInput = ({ title, value }) => {
 const StatePicker = () => {
   return (
     <DropDownPicker
-      items={[
-        { label: "UK", value: "uk" },
-        { label: "France", value: "france" },
-      ]}
+      items={auStates.map((item) => ({ label: item, value: item }))}
       containerStyle={{ height: 40 }}
     />
   );
 };
 
+interface FormValues {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  postcode: string;
+  state: string;
+  companyName: string;
+  abn: string;
+  hourlyRate: string;
+  insuranceExpiryDate: string;
+}
+
+const initialValues: FormValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  phone: "",
+  postcode: "",
+  state: "",
+  companyName: "",
+  abn: "",
+  hourlyRate: "",
+  insuranceExpiryDate: "",
+};
+
+const onSubmit = (values) => {
+  console.log("vals", values);
+};
+
 export default function App() {
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validationSchema: schema,
+  });
+
+  const formikHelper = (value: keyof FormValues) => ({
+    value: formik.values[value],
+    onChange: formik.handleChange(value),
+    onBlur: formik.handleBlur(value),
+    showError: formik.touched[value] && !!formik.errors[value],
+    error: formik.errors[value],
+  });
+
+  console.log('formik', formik.errors)
+  console.log('values', formik.values)
+
   return (
     <ScrollView style={styles.container}>
       <Text>edit account</Text>
-      <FormInput title="First Name" value="hi" />
-      <FormInput title="First Last" value="hi" />
-      <FormInput title="Email" value="hi" />
-      <FormInput title="Phone" value="hi" />
-      <FormInput title="Postcode" value="hi" />
+      <FormInput title="First Name" {...formikHelper("firstName")} />
+      <FormInput title="Last Name" {...formikHelper("lastName")} />
+      <FormInput title="Email" {...formikHelper("email")} />
+      <FormInput title="Phone" {...formikHelper("phone")} />
+      <FormInput title="Postcode" {...formikHelper("postcode")} />
       <StatePicker />
-      <FormInput title="State" value="hi" />
-      <FormInput title="Company Name" value="hi" />
-      <FormInput title="ABN" value="hi" />
-      <FormInput title="Hourly Rate (in $)" value="hi" />
-      <FormInput title="Insurance Expiry Date" value="hi" />
+      <FormInput title="Company Name" {...formikHelper("companyName")} />
+      <FormInput title="ABN" {...formikHelper("abn")} />
+      <FormInput title="Hourly Rate (in $)" {...formikHelper("hourlyRate")} />
+      <FormInput
+        title="Insurance Expiry Date"
+        {...formikHelper("insuranceExpiryDate")}
+      />
     </ScrollView>
   );
 }
