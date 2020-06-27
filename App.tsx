09a7b6327915b2771bed1eb013b15ec262c775f8
Nethan <nethan.tran@gmail.com>
2020-06-27 16:30:00 +1000

    add navigation pages to account and edit account

--- App.tsx
+++ App.tsx
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Account from "./screens/Account";
+import EditAccount from "./screens/EditAccount";
+
+const Stack = createStackNavigator();
+
+const AppNavigation = () => (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Account"
+        component={Account}
+        options={({ navigation }) => ({
+          title: "My Account",
+          headerRight: () => (
+            <Button
+              onPress={() => navigation.navigate("EditAccount")}
+              title="edit"
+            />
+          ),
+        })}
+      />
+      <Stack.Screen
+        name="EditAccount"
+        component={EditAccount}
+        options={{
+          title: "My Account",
+          headerRight: () => <Button onPress={() => {}} title="save" />,
+        }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+const App = () => <AppNavigation />;
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
